98664f970c8301cb84bd805dd60c972e92b8f88d
PoonamPatil99 <77851496+PoonamPatil99@users.noreply.github.com>
2021-01-27 01:54:26 +0530

    Add files via upload

---
+++ Day01.js
@@ -0,0 +1,112 @@
+// Edit your script here
+//var comapny = "Intimetec"
+//var classes = "Emertxe"
+//var location = "Bangalore"
+//var state = "karnataka"
+//var country = "india"
+//var carname = "Tata"
+//var bike = "Royal Enfield"
+//var city = "Bangalore"
+//var fooditem = "pavbhaji"
+//var snacks = "panipuri"
+//var TV = "sumsung"
+//var quote = "hardwork beats talent"
+//var parent = "Mother Father"
+
+
+function care()
+{
+	var emotion = "happyy"
+	var money = 5000
+	var support ="always supportive"
+	var hadurlunch = "yes"
+	console.log(emotion,money,support,hadurlunch)
+}
+care()
+
+function mother(emotion,caring)
+{
+	var salary = "5lakhs"
+	console.log(emotion,caring,salary)
+}
+mother("happiness","always she want u to be happy")
+
+function bdata(strength,emotion)
+{
+	var hadurbrkfst = "yes"
+	console.log(strength,emotion,hadurbrkfst)
+}
+bdata("helpful","care")
+
+function InfoC(companyname,designation,salary)
+{
+	var location = "hyderabad"
+	console.log(companyname,designation,salary,location)
+}
+InfoC("JC","programmer","3.2 or 320000")
+
+function emertxe(company,fees)
+{
+	var classtime = "8.30am"
+	console.log(company,fees,classtime)
+}
+emertxe("IT",35000)
+
+//-------------------------------------------------------------------
+class ceo {
+	name = "sundar pichai"
+	age = 51
+	salary = 234567
+}
+var sundarpichai = new ceo()
+console.log(sundarpichai.name)
+//------------------------------------------------------------------
+class humans{
+	emotion = "happiness"
+constructor()
+{
+	console.log(this.emotion)
+}
+}
+var venkatesh = new humans()
+//-----------------------------------------------------------------
+class inventor{
+constructor(langcreator){
+//document.writeln("inventor constructor")
+	this.c = langcreator
+	console.log(this.c)
+}
+}
+new inventor ("dennis ritchie")
+//----------------------------------------------------------------
+class company{
+	companyname = "JVT"
+	desgination = "programmer"
+	salary = "2.3 or 230000"
+	location = "bangalore"
+}
+var jvt = new company()
+console.log(jvt.companyname,jvt.desgination,jvt.salary,jvt.location)
+//-----------------------------------------------------------------
+class details{
+	Name = "Jai Venkateswara Technologies"
+	EducationalQualification = "B TECH MCA MBA BSC MSC"
+	DateOfBirth = "2/6/1988"
+	CommunicationAddress = "233/3 Ground floor new thippasandra"
+	ResidenceNumber = 08025216666
+	Mobile = 9900367097
+	Email = "venkatesh.db@gmail.com"
+	TotalITExp = "1 years"
+	SkillRelevantExperience = "C,CPP all Lang & 1 years"
+	ReportingManager = "Venkatesh"
+	PANCARDNO = "asu675"
+constructor()
+{
+	console.log(this.Name,this.EducationalQualification,this.DateOfBirth,this.CommunicationAddress,this.ResidenceNumber,this.Mobile,this.Email,this.TotalITExp,this.SkillRelevantExperience,this.ReportingManager,this.PANCARDNO)
+}
+}
+var details1 = new details()
+//--------------------------------------------------------------------
+
+
+
